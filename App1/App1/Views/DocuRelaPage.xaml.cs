e92c305e94169328f5363b00adc3f44bd26a8dc4
Sammyr Becerra Guzmán <sammyrito81@gmail.com>
2018-12-22 15:26:35 -0500

    Documentos Relacionados

---
+++ App1/App1/Views/DocuRelaPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using App1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DocuRelaPage : ContentPage
+	{
+		public DocuRelaPage (ObservableCollection<DocumentoItemViewModel> lista)
+		{
+            BindingContext = new ListaFacturasViewModel(lista);
+            InitializeComponent ();
+		}
+	}
+}
